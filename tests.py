7aa9a45fc421b3a89746a12430e14322ad523e96
Krish <krish.nava007@gmail.com>
2023-06-25 18:06:02 +0530

    :zap: added actions

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+from index import app  # replace with the name of your application file
+
+class FlaskAppTests(unittest.TestCase):
+    
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True 
+
+    def test_home_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
